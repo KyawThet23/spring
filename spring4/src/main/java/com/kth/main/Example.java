cdcc66694aabcae3257fe0e5969eb7be779d736e
kyawThetHtun <kyawthe235@gmail.com>
2024-05-13 14:44:37 +0630

    wiring with @autoWired

---
+++ spring4/src/main/java/com/kth/main/Example.java
@@ -0,0 +1,20 @@
+package com.kth.main;
+
+import com.kth.beans.Person;
+import com.kth.beans.Vehicle;
+import com.kth.config.ProjectConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Example {
+
+    public static void main(String[] args) {
+
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+        Person person = context.getBean(Person.class);
+        Vehicle vehicle = context.getBean(Vehicle.class);
+        System.out.println("Person name from Spring Context is: " + person.getName());
+        System.out.println("Vehicle name from Spring Context is: " + vehicle.getName());
+        System.out.println("Vehicle that Person own is: " + person.getVehicle());
+
+    }
+}
